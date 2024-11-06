6df57a6038e233c04d75bf4148b9bf983c4d055a
Israel Oyerinde <sogo.oyerinde@gmail.com>
2024-11-07 00:15:42 +0100

    first commit

---
+++ src/test/java/com/finappprojet/projectv/FinappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finappprojet.projectv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
